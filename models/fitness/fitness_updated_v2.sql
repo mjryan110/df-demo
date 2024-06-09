3377df91ccea7fd70bb9569c20a409359afbc5eb
mjrayn110 <mjrayn110@gmail.com>
2024-06-09 14:52:08 -0500

    test

--- models/fitness/fitness_updated_v2.sql
+++ models/fitness/fitness_updated_v2.sql
@@ -2,13 +2,29 @@
 
 -- {{ config(materialized='table') }}
 
+-- with fitness_data as (
+--     select
+--         id,
+--         swim,
+--         bike,
+--         run
+--     from {{ source('postgres', 'fitness') }}  
+-- )
+
+-- select
+--     id,
+--     swim,
+--     bike,
+--     run
+-- from fitness_data
+
 with fitness_data as (
     select
         id,
         swim,
         bike,
         run
-    from {{ source('postgres', 'fitness') }}  
+    from {{ ref('fitness') }}  
 )
 
 select
