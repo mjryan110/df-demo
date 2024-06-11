d681bf03ccf2ab68da7bbac1f6fa61c0ed50c88b
mjrayn110 <mjrayn110@gmail.com>
2024-06-11 16:56:18 -0500

    some changes to get the new table

--- models/fitness/fitness_updated_v2.sql
+++ models/fitness/fitness_updated_v2.sql
@@ -1,22 +1,6 @@
 -- models/fitness/fitness_updated_v2.sql
 
--- {{ config(materialized='table') }}
 
--- with fitness_data as (
---     select
---         id,
---         swim,
---         bike,
---         run
---     from {{ source('postgres', 'fitness') }}  
--- )
-
--- select
---     id,
---     swim,
---     bike,
---     run
--- from fitness_data
 
 {{ config(materialized='table') }}
 
