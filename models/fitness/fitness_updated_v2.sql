c0736773e50471305653ad5656263a711a9caa0e
mjrayn110 <mjrayn110@gmail.com>
2024-06-11 16:45:57 -0500

    changed the table I am pointing to

--- models/fitness/fitness_updated_v2.sql
+++ models/fitness/fitness_updated_v2.sql
@@ -1,19 +1,35 @@
 -- models/fitness/fitness_updated_v2.sql
 
+-- {{ config(materialized='table') }}
+
+-- with fitness_data as (
+--     select
+--         id,
+--         swim,
+--         bike,
+--         run
+--     from {{ source('postgres', 'fitness') }}  
+-- )
+
+-- select
+--     id,
+--     swim,
+--     bike,
+--     run
+-- from fitness_data
+
 {{ config(materialized='table') }}
 
 with fitness_data as (
     select
         id,
-        swim,
-        bike,
-        run
-    from {{ source('postgres', 'fitness') }}  
+        activity,
+        distance
+    from {{ source('postgres', 'fitnessData') }}  
 )
 
 select
     id,
-    swim,
-    bike,
-    run
+    activity,
+    distance
 from fitness_data
